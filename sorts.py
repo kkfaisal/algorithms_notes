a47cd223bfc86f2361cf4e67466ddb8b57515611
faisal kk <faisal@hifx.co.in>
2017-05-30 15:19:33 +0530

    quick sort added

--- sorts.py
+++ sorts.py
@@ -171,3 +171,28 @@ print  f
 Key To Remember :
 
 """
+
+def partition(array, begin, end):
+    pivot = begin
+    for i in range(begin+1, end+1):
+        if array[i] <= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
+
+
+def quicksort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    def _quicksort(array, begin, end):
+        if begin >= end:
+            return
+        pivot = partition(array, begin, end)
+        _quicksort(array, begin, pivot-1)
+        _quicksort(array, pivot+1, end)
+    return _quicksort(array, begin, end)
+
+array = [70, 20, 150, 11, 21, 17]
+quicksort(array)
+print array
